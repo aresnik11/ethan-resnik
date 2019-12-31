f78d643369cc4fd8da726c24d24420ab1d73303d
aresnik11 <aresnik11@gmail.com>
2019-12-31 11:24:28 -0500

    added photo gallery

--- src/components/CompMenu.js
+++ src/components/CompMenu.js
@@ -10,177 +10,177 @@ class CompMenu extends React.Component {
         });
     }
 
-  render() {
-    return (
-        <div>
-            <Menu
-                vertical
-                id="vert-cont"
-            >
-                <Menu.Item>
-                    <Menu.Header
-                        as="a"
-                        href="#orchestra"
-                        className="content-header"
-                    >
-                        Orchestra
-                    </Menu.Header>
-                    <Menu.Menu>
-                        <Menu.Item
+    render() {
+        return (
+            <div>
+                <Menu
+                    vertical
+                    id="vert-cont"
+                >
+                    <Menu.Item>
+                        <Menu.Header
                             as="a"
-                            href="#snow-pond"
-                            name='Snow Pond Overture'
-                            active={this.state.activeItem === 'Snow Pond Overture'}
-                            onClick={this.handleItemClick}
-                        />
-                        <Menu.Item
-                            as="a"
-                            href="#rush"
-                            name='Rush'
-                            active={this.state.activeItem === 'Rush'}
-                            onClick={this.handleItemClick}
-                        />
-                    </Menu.Menu>
-                </Menu.Item>
+                            href="#orchestra"
+                            className="content-header"
+                        >
+                            Orchestra
+                        </Menu.Header>
+                        <Menu.Menu>
+                            <Menu.Item
+                                as="a"
+                                href="#snow-pond"
+                                name='Snow Pond Overture'
+                                active={this.state.activeItem === 'Snow Pond Overture'}
+                                onClick={this.handleItemClick}
+                            />
+                            <Menu.Item
+                                as="a"
+                                href="#rush"
+                                name='Rush'
+                                active={this.state.activeItem === 'Rush'}
+                                onClick={this.handleItemClick}
+                            />
+                        </Menu.Menu>
+                    </Menu.Item>
 
-                <Menu.Item>
-                    <Menu.Header
-                        as="a"
-                        href="#concert-band"
-                    >
-                        Concert Band
-                    </Menu.Header>
-                    <Menu.Menu>
-                        <Menu.Item
+                    <Menu.Item>
+                        <Menu.Header
                             as="a"
-                            href="#arrival"
-                            name='Arrival'
-                            active={this.state.activeItem === 'Arrival'}
-                            onClick={this.handleItemClick}
-                        />
-                    </Menu.Menu>
-                </Menu.Item>
+                            href="#concert-band"
+                        >
+                            Concert Band
+                        </Menu.Header>
+                        <Menu.Menu>
+                            <Menu.Item
+                                as="a"
+                                href="#arrival"
+                                name='Arrival'
+                                active={this.state.activeItem === 'Arrival'}
+                                onClick={this.handleItemClick}
+                            />
+                        </Menu.Menu>
+                    </Menu.Item>
 
-                <Menu.Item>
-                    <Menu.Header
-                        as="a"
-                        href="#string-orchestra"
-                    >
-                        String Orchestra
-                    </Menu.Header>
-                    <Menu.Menu>
-                        <Menu.Item
+                    <Menu.Item>
+                        <Menu.Header
                             as="a"
-                            href="#blue-moonlight"
-                            name='Blue Moonlight'
-                            active={this.state.activeItem === 'Blue Moonlight'}
-                            onClick={this.handleItemClick}
-                        />
-                    </Menu.Menu>
-                </Menu.Item>
+                            href="#string-orchestra"
+                        >
+                            String Orchestra
+                        </Menu.Header>
+                        <Menu.Menu>
+                            <Menu.Item
+                                as="a"
+                                href="#blue-moonlight"
+                                name='Blue Moonlight'
+                                active={this.state.activeItem === 'Blue Moonlight'}
+                                onClick={this.handleItemClick}
+                            />
+                        </Menu.Menu>
+                    </Menu.Item>
 
-                <Menu.Item>
-                    <Menu.Header
-                        as="a"
-                        href="#chamber-music"
-                    >
-                        Chamber Music
-                    </Menu.Header>
-                    <Menu.Menu>
-                        <Menu.Item
+                    <Menu.Item>
+                        <Menu.Header
                             as="a"
-                            href="#lake-kawaguchi"
-                            name='Lake Kawaguchi at Dawn'
-                            active={this.state.activeItem === 'Lake Kawaguchi at Dawn'}
-                            onClick={this.handleItemClick}
+                            href="#chamber-music"
                         >
-                            Lake Kawaguchi at Dawn
-                        </Menu.Item>
-                        <Menu.Item
-                            as="a"
-                            href="#dance-phantoms"
-                            name='Dance of the Phantoms'
-                            active={this.state.activeItem === 'Dance of the Phantoms'}
-                            onClick={this.handleItemClick}
-                        >
-                            Dance of the Phantoms
-                        </Menu.Item>
-                        <Menu.Item
-                            as="a"
-                            href="#winter-flashbacks"
-                            name='Winter Flashbacks'
-                            active={this.state.activeItem === 'Winter Flashbacks'}
-                            onClick={this.handleItemClick}
-                        />
-                        <Menu.Item
-                            as="a"
-                            href="#night-rainforest"
-                            name='Night in the Rainforest'
-                            active={this.state.activeItem === 'Night in the Rainforest'}
-                            onClick={this.handleItemClick}
-                        >
-                            Night in the Rainforest
-                        </Menu.Item>
-                        <Menu.Item
-                            as="a"
-                            href="#graduation-eve"
-                            name='Graduation Eve'
-                            active={this.state.activeItem === 'Graduation Eve'}
-                            onClick={this.handleItemClick}
-                        />
-                    </Menu.Menu>
-                </Menu.Item>
+                            Chamber Music
+                        </Menu.Header>
+                        <Menu.Menu>
+                            <Menu.Item
+                                as="a"
+                                href="#lake-kawaguchi"
+                                name='Lake Kawaguchi at Dawn'
+                                active={this.state.activeItem === 'Lake Kawaguchi at Dawn'}
+                                onClick={this.handleItemClick}
+                            >
+                                Lake Kawaguchi at Dawn
+                            </Menu.Item>
+                            <Menu.Item
+                                as="a"
+                                href="#dance-phantoms"
+                                name='Dance of the Phantoms'
+                                active={this.state.activeItem === 'Dance of the Phantoms'}
+                                onClick={this.handleItemClick}
+                            >
+                                Dance of the Phantoms
+                            </Menu.Item>
+                            <Menu.Item
+                                as="a"
+                                href="#winter-flashbacks"
+                                name='Winter Flashbacks'
+                                active={this.state.activeItem === 'Winter Flashbacks'}
+                                onClick={this.handleItemClick}
+                            />
+                            <Menu.Item
+                                as="a"
+                                href="#night-rainforest"
+                                name='Night in the Rainforest'
+                                active={this.state.activeItem === 'Night in the Rainforest'}
+                                onClick={this.handleItemClick}
+                            >
+                                Night in the Rainforest
+                            </Menu.Item>
+                            <Menu.Item
+                                as="a"
+                                href="#graduation-eve"
+                                name='Graduation Eve'
+                                active={this.state.activeItem === 'Graduation Eve'}
+                                onClick={this.handleItemClick}
+                            />
+                        </Menu.Menu>
+                    </Menu.Item>
 
-                <Menu.Item>
-                    <Menu.Header
-                        as="a"
-                        href="#solo"
-                    >
-                        Solo
-                    </Menu.Header>
-                    <Menu.Menu>
-                        <Menu.Item
+                    <Menu.Item>
+                        <Menu.Header
                             as="a"
-                            href="#promenade"
-                            name='Une Promenade à travers le Metropolitan'
-                            active={this.state.activeItem === 'Une Promenade à travers le Metropolitan'}
-                            onClick={this.handleItemClick}
+                            href="#solo"
                         >
-                            Une Promenade à travers le Metropolitan
-                        </Menu.Item>
-                        <Menu.Item
-                            as="a"
-                            href="#waltz5"
-                            name='Waltz No. 5'
-                            active={this.state.activeItem === 'Waltz No. 5'}
-                            onClick={this.handleItemClick}
-                        >
-                            Waltz No. 5
-                        </Menu.Item>
-                        <Menu.Item
-                            as="a"
-                            href="#prelude2"
-                            name='Prelude No. 2'
-                            active={this.state.activeItem === 'Prelude No. 2'}
-                            onClick={this.handleItemClick}
-                        >
-                            Prelude No. 2
-                        </Menu.Item>
-                        <Menu.Item
-                            as="a"
-                            href="#nocturne1"
-                            name='Nocturne No. 1'
-                            active={this.state.activeItem === 'Nocturne No. 1'}
-                            onClick={this.handleItemClick}
-                        >
-                            Nocturne No. 1
-                        </Menu.Item>
-                    </Menu.Menu>
-                </Menu.Item>
-            </Menu>
-        </div>
-    )
-  }
+                            Solo
+                        </Menu.Header>
+                        <Menu.Menu>
+                            <Menu.Item
+                                as="a"
+                                href="#promenade"
+                                name='Une Promenade à travers le Metropolitan'
+                                active={this.state.activeItem === 'Une Promenade à travers le Metropolitan'}
+                                onClick={this.handleItemClick}
+                            >
+                                Une Promenade à travers le Metropolitan
+                            </Menu.Item>
+                            <Menu.Item
+                                as="a"
+                                href="#waltz5"
+                                name='Waltz No. 5'
+                                active={this.state.activeItem === 'Waltz No. 5'}
+                                onClick={this.handleItemClick}
+                            >
+                                Waltz No. 5
+                            </Menu.Item>
+                            <Menu.Item
+                                as="a"
+                                href="#prelude2"
+                                name='Prelude No. 2'
+                                active={this.state.activeItem === 'Prelude No. 2'}
+                                onClick={this.handleItemClick}
+                            >
+                                Prelude No. 2
+                            </Menu.Item>
+                            <Menu.Item
+                                as="a"
+                                href="#nocturne1"
+                                name='Nocturne No. 1'
+                                active={this.state.activeItem === 'Nocturne No. 1'}
+                                onClick={this.handleItemClick}
+                            >
+                                Nocturne No. 1
+                            </Menu.Item>
+                        </Menu.Menu>
+                    </Menu.Item>
+                </Menu>
+            </div>
+        )
+    }
 }
 
 export default CompMenu
--- src/components/Photos.js
+++ src/components/Photos.js
@@ -4,31 +4,91 @@ import performance2 from '../assets/performance2.jpeg'
 import npr_article from '../assets/npr_article.jpeg'
 import piano_performance from '../assets/piano_performance.jpeg'
 import performance3 from '../assets/performance3.jpeg'
-import performance4 from '../assets/performance4.png'
+import performance4 from '../assets/performance4.jpg'
 import poster from '../assets/poster.jpg'
 import ethan_music from '../assets/ethan_music.jpg'
+import npr from '../assets/npr.jpg'
+import about from '../assets/about.jpg'
+import homepage from '../assets/homepage.jpg'
+import Gallery from 'react-grid-gallery';
 
 const Photos = () => {
+    const images = [
+        {
+            src: performance,
+            thumbnail: performance,
+            thumbnailWidth: 450,
+            thumbnailHeight: 245
+        },
+        {
+            src: ethan_music,
+            thumbnail: ethan_music,
+            thumbnailWidth: 450,
+            thumbnailHeight: 534
+        },
+        {
+            src: performance2,
+            thumbnail: performance2,
+            thumbnailWidth: 450,
+            thumbnailHeight: 245
+        },
+
+        {
+            src: piano_performance,
+            thumbnail: piano_performance,
+            thumbnailWidth: 450,
+            thumbnailHeight: 298
+        },
+
+        {
+            src: performance4,
+            thumbnail: performance4,
+            thumbnailWidth: 450,
+            thumbnailHeight: 245
+        },
+        {
+            src: poster,
+            thumbnail: poster,
+            thumbnailWidth: 450,
+            thumbnailHeight: 534
+        },
+
+        {
+            src: npr,
+            thumbnail: npr,
+            thumbnailWidth: 450,
+            thumbnailHeight: 534
+        },
+        {
+            src: about,
+            thumbnail: about,
+            thumbnailWidth: 450,
+            thumbnailHeight: 245
+        },
+        {
+            src: performance3,
+            thumbnail: performance3,
+            thumbnailWidth: 450,
+            thumbnailHeight: 245
+        },
+
+        {
+            src: homepage,
+            thumbnail: homepage,
+            thumbnailWidth: 450,
+            thumbnailHeight: 298
+        }
+    ]
     return (
         <div className="white-container">
             <h1 className="heading">Take a Look!</h1>
             <br/>
-            <img src={performance} alt="Performance" className="photo-large" />
-            <br/><br/>
-            <img src={performance2} alt="Performance" className="photo-large" />
-            <br/><br/>
-            <img src={npr_article} alt="NPR From the Top Article" className="photo-small" />
-            <br/><br/>
-            <img src={piano_performance} alt="Piano Performance" className="photo-large" />
-            <br/><br/>
-            <img src={performance3} alt="Performance" className="photo-large" />
-            <br/><br/>
-            <img src={performance4} alt="Performance" className="photo-large" />
-            <br/><br/>
-            <img src={poster} alt="Performance" className="photo-small" />
-            <br/><br/>
-            <img src={ethan_music} alt="Ethan Resnik" className="photo-small" />
-
+            <Gallery
+                images={images}
+                enableImageSelection={false}
+                backdropClosesModal={true}
+                rowHeight={300}
+            />
         </div>
     )
 }
