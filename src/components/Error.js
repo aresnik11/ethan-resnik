b9842993b53e62a8b5b5e571d97f53146ae5d4ad
aresnik11 <aresnik11@gmail.com>
2019-12-31 14:32:05 -0500

    add white container for 404

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,11 +3,11 @@ import Notes from './Notes'
 
 const Error = () => {
     return (
-        <>
+        <div className="white-container">
             <h1 className="heading">Page Not Found</h1>
             <br/><br/><br/>
             <Notes />
-        </>
+        </div>
     )
 }
 
