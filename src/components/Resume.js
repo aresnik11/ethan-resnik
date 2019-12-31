68e064e5d7db2c9c8bf446d61315206c389ce976
aresnik11 <aresnik11@gmail.com>
2019-12-31 14:29:37 -0500

    add view resume button

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import resume from '../assets/Ethan-Resnik-Resume.pdf'
+import { Button } from 'semantic-ui-react'
 
 const Resume = () => {
     return (
         <>
-            <iframe src={resume + '#view=FitH'} title="Ethan Resnik Resume" id="resume"></iframe>
+            <iframe src={resume} title="Ethan Resnik Resume" id="resume"></iframe>
+            <br/><br/><br/>
+            <Button
+                color='black'
+                content="View Resume"
+                size="big"
+                as="a"
+                href={resume}
+                target="_blank"
+                rel="noopener noreferrer"
+            />
         </>
     )
 }
