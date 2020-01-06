13a86b6f8e102487886bc62c3a06e67a8094af09
aresnik11 <aresnik11@gmail.com>
2020-01-06 15:57:22 -0500

    removed icons

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,12 +7,12 @@ const Homepage = () => {
         <div className="white-container">
             <h1 id="name">Ethan Resnik</h1>
             <h1 id="subheader">Composer, Pianist, Saxophonist</h1>
-            <a href="https://www.youtube.com/channel/UCKISvqGCXDr0hbyTWrn9Jlg" target="_blank" rel="noopener noreferrer">
+            {/* <a href="https://www.youtube.com/channel/UCKISvqGCXDr0hbyTWrn9Jlg" target="_blank" rel="noopener noreferrer">
                 <Icon name="youtube" size="huge" />
             </a>
             <a href="https://www.instagram.com/ethresnik" target="_blank" rel="noopener noreferrer">
                 <Icon name="instagram" size="huge" />
-            </a>
+            </a> */}
             {/* <br/><br/><br/>
             <img src={homepage} alt="Ethan Resnik" id="home-photo" /> */}
         </div>
